bd71469a372d0a3080ad65151d3bd77839f8584c
Banacount <uniquejmal@gmail.com>
2026-01-23 06:59:10 +0800

    Did schoolwork week 2 yey

---
+++ week_2(variables-and-datatypes)/main.py
@@ -0,0 +1,17 @@
+fullname = "John Rus"
+age = 19
+height_meters = 1.778
+is_working = False
+
+# Output hehe
+print(f"Fullname: {fullname}")
+print(type(fullname))
+
+print(f"\nAge: {age}")
+print(type(age))
+
+print(f"\nHeight in meters: {height_meters}")
+print(type(height_meters))
+
+print(f"\nCurrently working?: {is_working}")
+print(type(is_working))
